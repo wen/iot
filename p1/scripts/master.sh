ab1cb6cb0c99148e3982e13de67a5b0bf033e816
Ye WEN <yewen@student.42.fr>
2022-04-10 21:26:34 +0200

    update master.sh

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,4 +1,5 @@
 curl -sfL https://get.k3s.io | sh -s - server \
 	--write-kubeconfig-mode 644 \
+	--advertise-address=192.168.42.110 \
 	--node-ip=192.168.42.110
 cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts
