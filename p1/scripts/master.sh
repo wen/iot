3f9bf3ee28576b41aa758e5c2ff38ed0d79618eb
Ye WEN <yewen@student.42.fr>
2022-04-17 15:25:40 +0200

    Change p1 to cho & aslan style

--- p1/scripts/master.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /root/.ssh
-cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
-
-curl -sfL https://get.k3s.io | sh -s - server \
-	--write-kubeconfig-mode 644 \
-	--advertise-address "192.168.42.110" \
-	--node-ip "192.168.42.110"
