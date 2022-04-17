1d70789a08bd8ea251ddf40d53628635458a6314
Sungyong Cho <to.sungyongcho@gmail.com>
2022-04-17 14:41:48 +0000

    add different argocd mainfest repo for sucho

---
+++ p3_sucho/scripts/create-cluster-and-install.sh
@@ -0,0 +1,45 @@
+echo "creating cluster"
+sudo k3d cluster create -p 8080:80@loadbalancer -p 8888:30888@loadbalancer
+echo "created cluster, waiting..."
+sleep 5
+
+echo "creating namespaces"
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+echo "created namespaces, waiting..."
+sleep 3
+
+echo "installing argocd"
+sudo kubectl apply -n argocd -f ../confs/install.yaml
+echo "installed argocd, waiting..."
+sleep 3
+
+echo "waiting the pods to be ready"
+sudo kubectl wait -n argocd --for=condition=Ready pods --all
+echo "pods ready, waiting..."
+sleep 3
+
+echo "installing ingress"
+sudo kubectl apply -f ../confs/ingress.yaml -n argocd
+echo "installed ingress, waiting..."
+sleep 3
+
+echo "changing default password to argocd"
+sudo kubectl -n argocd patch secret argocd-secret \
+  -p '{"stringData": {
+    "admin.password": "$2a$12$px3TAQ7CeurPy.dBLjyjD.W6hE9O3wNdpI.OKaoxxDDs1IVMPKd5O",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }}'
+echo "changed default password to argocd, waiting..."
+sleep 3
+
+echo "installing project to argocd"
+sudo kubectl apply -f ../confs/project.yaml -n argocd
+echo "installed project to argocd"
+sleep 3
+
+echo "installing application to argocd"
+sudo kubectl apply -f ../confs/application.yaml -n argocd
+echo "installed application to argocd"
+
+echo "DONE!"
