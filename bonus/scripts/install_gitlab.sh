a184bfc32e160d60c63ec6e675605290b4975ed3
Sungyong Cho <to.sungyongcho@gmail.com>
2022-04-18 20:46:27 +0000

    fixing bonus

--- bonus/scripts/install_gitlab.sh
+++ bonus/scripts/install_gitlab.sh
@@ -5,14 +5,26 @@ IP=$(hostname -I | awk '{print $2}')
 echo "start - install gitlab - "$IP
 
 echo "[1]: install gitlab"
-apt-get update -qq >/dev/null
-apt-get install -qq -y vim git wget curl git >/dev/null
+
+sudo locale-gen "en_US.UTF-8"
+sudo sh -c 'echo "LANG=en_US.utf-8\nLC_ALL=en_US.utf-8\nLC_CTYPE=en_US.UTF-8" > /etc/default/locale'
+
+echo "export LANGUAGE=en_US.UTF-8\nexport LANG=en_US.UTF-8\nexport LC_ALL=en_US.UTF-8">>~/.bash_profile
+
+sudo apt-get update -qq >/dev/null
+sudo apt-get install -qq -y vim git wget curl git >/dev/null
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-apt-get update -qq >/dev/null
+sudo apt-get update -qq >/dev/null
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_us.UTF-8
-apt install -y gitlab-ce
-gitlab-ctl reconfigure
+sudo apt install -y gitlab-ce
+
+########################
+# change access address here
+# add portnumber 9999
+###################
+
+sudo gitlab-ctl reconfigure
 echo "END - install gitlab"
 
 #sudo dpkg-reconfugre locales
