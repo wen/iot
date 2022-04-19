695a8db6b8b6527536ec981be6a39a4e105c49dc
Sungyong Cho <to.sungyongcho@gmail.com>
2022-04-19 00:21:15 +0000

    fix debian locale setting

--- bonus/scripts/install_gitlab.sh
+++ bonus/scripts/install_gitlab.sh
@@ -6,17 +6,20 @@ echo "start - install gitlab - "$IP
 
 echo "[1]: install gitlab"
 
-sudo locale-gen "en_US.UTF-8"
-sudo sh -c 'echo "LANG=en_US.utf-8\nLC_ALL=en_US.utf-8\nLC_CTYPE=en_US.UTF-8" > /etc/default/locale'
+# https://serverfault.com/questions/362903/how-do-you-set-a-locale-non-interactively-on-debian-ubuntu
 
-echo "export LANGUAGE=en_US.UTF-8\nexport LANG=en_US.UTF-8\nexport LC_ALL=en_US.UTF-8">>~/.bash_profile
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+
+
+sudo locale-gen --purge en_US.UTF-8
+sudo sh -c 'echo "LANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8\nLC_ALL=en_US.UTF-8\nLC_CTYPE=en_US.UTF-8" > /etc/default/locale'
+
+echo "export LANGUAGE=en_US.UTF-8\nexport LANG=en_US.UTF-8\nexport LC_ALL=en_US.UTF-8\nexport LC_CTYPE=en_US.UTF-8">>~/.bash_profile
 
 sudo apt-get update -qq >/dev/null
 sudo apt-get install -qq -y vim git wget curl git >/dev/null
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 sudo apt-get update -qq >/dev/null
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_us.UTF-8
 sudo apt install -y gitlab-ce
 
 # add portnumber 9999
